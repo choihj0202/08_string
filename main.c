b22bcb29fa45bc8289416de1720db47970d65488
choihj0202 <helenchoi22@naver.com>
2018-11-22 16:36:36 +0900

    exercise6

--- main.c
+++ main.c
@@ -1,35 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	
 	FILE *fp1 = NULL;
-	FILE *fp2 = NULL;
-	char input;
+	char word[100];
 	char original[100];
-	char copy[100];
-	int i=0;
+	char input[100];
 	
 	printf("original file : ");
 	scanf("%s", original);
-	printf("copy file : ");
-	scanf("%s", copy);
 	
 	fp1 = fopen(original, "r");	
-	fp2 = fopen(copy, "w");
+	
+	printf("word to find : ");
+	scanf("%s", word);
 
-	while ((input = fgetc(fp1)) != EOF)
+	while ( fgets(input, 100, fp1) != NULL)
 	{
-		fputc(input, fp2);
-		i++;
+		if (strncmp(input, word, strlen(word)) == 0)
+		{
+			printf("find a word %s\n", input);
+		}
 	}
 	
-	printf("copy success! (%i bytes copied)", i);
+	printf("Search done!\n");
 	
 	fclose(fp1);
-	fclose(fp2);
 	
 	return 0;
 }
